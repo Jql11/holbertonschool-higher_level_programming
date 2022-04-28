a9896b18e9a0deef5a26d2b8c8ab42acc00bd978
jql11 <4325@holbertonschool.com>
2022-04-27 20:34:51 -0700

    elimate s when there is one argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if (len(sys.argv) != 1):
-        print("{} arguments:".format(len(sys.argv) - 1))
-    else:
+    if (len(sys.argv) == 1):
         print("0 argument.")
+    elif (len(sys.argv) == 2):
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
     for i, sys.argv in enumerate(sys.argv[1:]):
         print(i+1, ":", sys.argv)
