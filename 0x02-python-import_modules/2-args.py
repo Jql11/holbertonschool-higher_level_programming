720a4d501217171430d229b20b940789740fb1fc
jql11 <4325@holbertonschool.com>
2022-04-27 19:05:20 -0700

    add name=main

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import sys
-if (len(sys.argv) != 1):
-    print("{} arguments:".format(len(sys.argv) - 1))
-else:
-    print("0 argument.")
-for i, sys.argv in enumerate(sys.argv[1:]):
-    print(i+1, ":", sys.argv)
+if __name__ == "__main__":
+    import sys
+    if (len(sys.argv) != 1):
+        print("{} arguments:".format(len(sys.argv) - 1))
+    else:
+        print("0 argument.")
+    for i, sys.argv in enumerate(sys.argv[1:]):
+        print(i+1, ":", sys.argv)
