e0492c0fa29f29cff845b23771938e98f2daf23b
jql11 <4325@holbertonschool.com>
2022-05-01 19:16:57 -0700

    delete space at the end of each row

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in row:
-            print(i, end=' ')
+            if i is not row[len(row) - 1]:
+                print(i, end=' ')
+            else:
+                print(i, end='')
         print()
