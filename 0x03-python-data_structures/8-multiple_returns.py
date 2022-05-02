1b5807a2f9be235fd5592da08367fc0dd1a68864
jql11 <4325@holbertonschool.com>
2022-05-01 21:51:48 -0700

    remove else

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if not len(sentence):
-        return (len(sentnece), None)
-    else:
-        return (len(sentence), sentence[0])
+        sentence[0] = None
+    return (len(sentence), sentence[0])
