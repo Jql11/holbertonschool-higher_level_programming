8f3c195723a06fb511e9be3c931461fbcd009e46
jql11 <4325@holbertonschool.com>
2022-05-04 05:44:32 -0700

    add str type edge case

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
-        return 0
-    if roman_string == "":
+    if not roman_string or type(roman_string) != str:
         return 0
     dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     int_val = 0
