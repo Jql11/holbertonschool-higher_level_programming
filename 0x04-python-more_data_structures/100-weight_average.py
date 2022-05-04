35163e54020ad302c88b2e2b666a1cc68492ee65
jql11 <4325@holbertonschool.com>
2022-05-04 06:05:56 -0700

    first commit

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,9 +2,6 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    score = 0
-    weight = 0
-    for i in my_list:
-        score += (i[0] * i[1])
-        weight += (i[1])
+    score = sum(a * b for a, b in my_list)
+    weight = sum(b for a, b in my_list)
     return score/weight
