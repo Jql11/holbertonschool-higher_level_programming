7ce150d053d514edc728e02b10b0ad6594a71f9e
jql11 <4325@holbertonschool.com>
2022-05-22 05:30:09 -0700

    fix space error

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -9,5 +9,9 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     for char in ".?:":
-        text = text.replace(char + " ", char + "\n\n")
-    print(text)
+        text = text.replace(char, char + "\n\n")
+    new_list = []
+    for line in text.split('\n'):
+        new_list.append(line.lstrip(' '))
+    new_text = "\n".join(new_list)
+    print(new_text, end="")
