71245b13fc87454dcda3ade9d6d8ed79dd180f3b
jql11 <4325@holbertonschool.com>
2022-05-30 03:00:02 -0700

    import 7-base_geometry

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -7,22 +7,7 @@ the following rectangle description: [Rectangle] <width>/<height>
 """
 
 
-class BaseGeometry:
-    """class"""
-
-    def area(self):
-        """rasie exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        validates value
-        assume name always string
-        """
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
