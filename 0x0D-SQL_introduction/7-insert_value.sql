06a7cd1160cd485a1bbf3be13a369311c7b72cf9
jql11 <4325@holbertonschool.com>
2022-07-04 22:02:24 -0700

    change syntax

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- inserts a new row in the table first_table from hbtn_0c_0 in MySQL server.
 -- new row: id = 89; name = Best School
-INSERT INTO first_table (`id`, `name`) VALUES (89, 'Best School');
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
