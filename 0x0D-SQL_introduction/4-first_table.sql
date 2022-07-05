c5ec96ac6bab495ced0b2b693d07bee9c494fbdf
jql11 <4325@holbertonschool.com>
2022-07-04 21:02:42 -0700

    fix syntax

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- creates a table called first_table in the current database in your MySQL server.
-CREATE TABLE IF NOT EXISTS FIRST_TABLE (ID INT, NAME VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (ID INT, NAME VARCHAR(256));
