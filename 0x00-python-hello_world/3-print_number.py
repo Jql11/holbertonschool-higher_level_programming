103fc6fd48544b2d9b30efdd97852aa0c4363568
Jql11 <4325@holbertonschool.com>
2022-07-06 11:37:11 +1000

    add file

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""comment."""
 from calendar import c
 
 
 number = 98
 print(f"{number} Battery street")
+
+
 def a():
+    """comment."""
     a is b
     return c
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,2 @@
+0x0E. SQL - More queries
+15 mandatories and 5 advanced tasks
