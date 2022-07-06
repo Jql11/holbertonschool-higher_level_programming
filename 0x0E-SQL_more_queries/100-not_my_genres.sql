34a2c3a4950907ae2fe7ab0d6c2458a516ac247e
jql11 <4325@holbertonschool.com>
2022-07-06 04:57:25 -0700

    add file

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,4 @@
+-- uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+SELECT name FROM tv_genres WHERE name NOT IN
+(SELECT tv_genres.name AS 'name' FROM tv_genres LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id WHERE tv_shows.title = 'Dexter' ORDER BY name)
+ORDER BY name;
