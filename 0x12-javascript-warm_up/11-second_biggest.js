45bed7afd65ee12bed80fa10bfa34237e5533af6
Jql11 <4325@holbertonschool.com>
2022-08-01 16:44:31 +1000

    change method to comply checker

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,22 @@
 #!/usr/bin/node
+function nextBiggest (arr) {
+  let max = -Infinity; let result = -Infinity;
+
+  for (const value of arr) {
+    const nr = Number(value);
+
+    if (nr > max) {
+      [result, max] = [max, nr]; // save previous max
+    } else if (nr < max && nr > result) {
+      result = nr; // new second biggest
+    }
+  }
+
+  return result;
+}
+
 if (process.argv.length < 4) {
   console.log(0);
 } else {
-  console.log(process.argv.sort(function (a, b) { return b - a; })[3]);
+  console.log(nextBiggest(process.argv));
 }
