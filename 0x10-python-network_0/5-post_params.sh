0a69aeb1ab6cb71f2d0221b8b7337db22f83adc9
jql11 <4325@holbertonschool.com>
2022-08-17 17:36:52 -0700

    add -s

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a POST request, A variable email must be sent with the value test@gmail.comand A variable subject must be sent with the value I will always be here for PLD
-curl -X POST "$1" -F 'email=test@gmail.com' -F 'subject=I will always be here for PLD'
+curl -s -X POST "$1" -F 'email=test@gmail.com' -F 'subject=I will always be here for PLD'
