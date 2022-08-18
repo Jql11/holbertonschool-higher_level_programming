3e7bc61afc90cb9bda47d9e08e40549b3a465135
jql11 <4325@holbertonschool.com>
2022-08-17 17:32:53 -0700

    change awk

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes in a URL and displays all HTTP methods the server will accept.
-curl -s -i -L -X OPTIONS "$1" | grep -i Allow | awk '{print $2, $3, $4}'
+curl -s -i -L -X OPTIONS "$1" | grep -i Allow | awk -F': ''{print $2}'
