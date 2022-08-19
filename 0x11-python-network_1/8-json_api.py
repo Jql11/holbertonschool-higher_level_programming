4b0bf4bc8f420fb19fa677a292b53482fbf72e1a
jql11 <4325@holbertonschool.com>
2022-08-18 19:37:16 -0700

    test

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter.
-The letter must be sent in the variable q
-If no argument is given, set q=""
-display the id and name like this: [<id>] <name>
-"""
+""" using requests package - takes in a letter and sends a POST request to
+ http://0.0.0.0:5000/search_user with the letter as a parameter """
+from requests import post
 from sys import argv
-import requests
 
 
 if __name__ == "__main__":
-    if len(argv) < 2:
-        letter = ""
+    if len(argv) > 1:
+        data = {'q': argv[1]}
     else:
-        letter = argv[1]
-    url = 'http://0.0.0.0:5000/search_user'
-    payload = {'q': letter}
-    r = requests.post(url, data=payload)
+        data = {'q': ""}
 
+    response = post('http://0.0.0.0:5000/search_user', data)
     try:
-        dic = r.json()
-        if dic:
-            print("[{}] {}".format(dic['id'], dic['name']))
+        j_response = response.json()
+        if len(j_response.keys()) > 0:
+            print("[{}] {}".format(j_response['id'], j_response['name']))
         else:
             print("No result")
-    except ValueError as e:
+    except:
         print("Not a valid JSON")
