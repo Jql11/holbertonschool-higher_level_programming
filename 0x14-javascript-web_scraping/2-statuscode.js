889bd70d640f35c9ac5b1dc717c418bfd2dbc176
Jql11 <4325@holbertonschool.com>
2022-09-13 12:08:05 +1000

    change method

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
-const https = require('https');
-const url = process.argv[2];
 
-https.get(url, function (res) {
-  console.log('code: ', res.statusCode);
+let url = process.argv[2];
+const request = require('request');
+
+request(url, function (err, response) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('code: ' + response.statusCode);
 });
