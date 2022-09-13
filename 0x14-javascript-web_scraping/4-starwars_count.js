4cbe79acb2c64e8e4d1d6c182a8569a90a361344
Jql11 <4325@holbertonschool.com>
2022-09-13 16:10:36 +1000

    add file

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const axios = require('axios');
+const url = process.argv[2];
+
+axios
+  .get(url)
+  .then(response => {
+    const films = response.data.results;
+    let count = 0;
+    for (const features of films) {
+      for (const people of features.characters) {
+        if (people === 'https://swapi-api.hbtn.io/api/people/18/') {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  })
+  .catch(error => {
+    console.error(error);
+  });
